b67921224787db47c14657a8dc036421bd2d5a99
rithishcodespace <rithishcodespace@gmail.com>
2025-05-01 17:05:24 +0530

    resolved path issue

--- frontend/src/components/Connections.jsx
+++ frontend/src/components/Connections.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect,useState } from 'react';
 import axios from 'axios';
-import FeedCard from './feedCard';
+import FeedCard from "./FeedCard"
 
 const Connections = () => {
 
