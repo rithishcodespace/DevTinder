029d8850fab3f28f2bdabcec8a99f72272ac738b
rithishcodespace <rithishcodespace@gmail.com>
2025-05-01 17:08:57 +0530

    hava

--- frontend/src/components/Connections.jsx
+++ frontend/src/components/Connections.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect,useState } from 'react';
 import axios from 'axios';
-import FeedCard from "./FeedCard"
+import FeedCard from './feedCard';
 
 const Connections = () => {
 
