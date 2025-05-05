a12be1f38a6d52dae037c67d5c7b8a56b60cdbbe
rithishcodespace <rithishcodespace@gmail.com>
2025-05-05 11:12:08 +0530

    env

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
+console.log(process.env.MONGO_URI); 
 const mongoose = require("mongoose");
 
 const connectDB = async () =>{
-   await mongoose.connect(process.env.MONGO_URI);
+   await mongoose.connect("mongodb+srv://rithishcodespace:Rithish%402006@rithish.cvr15.mongodb.net/?retryWrites=true&w=majority&appName=Rithish");
 }
 
 module.exports = connectDB;
