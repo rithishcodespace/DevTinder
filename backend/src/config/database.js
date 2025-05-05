4e632489a55bed1dd0653c60cd910a9751019a9c
rithishcodespace <rithishcodespace@gmail.com>
2025-05-05 11:29:13 +0530

    hi lala

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+console.log(process.env.MONGO_URI); 
 const mongoose = require("mongoose");
 
 const connectDB = async () =>{
-   await mongoose.connect("mongodb+srv://rithishcodespace:Rithish%402006@rithish.cvr15.mongodb.net/?retryWrites=true&w=majority&appName=Rithish");
+   await mongoose.connect(process.env.MONGO_URI);
 }
 
 module.exports = connectDB;
